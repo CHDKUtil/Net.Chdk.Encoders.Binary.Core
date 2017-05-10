e980355fa44fb26e3dfe4eaf78b63816821cc537
Dmitry Shechtman <damnian@gmail.com>
2017-05-11 02:36:28 +0300

    Add solution

---
+++ src/Net.Chdk.Encoders.Binary.Core/IBinaryDecoder.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace Net.Chdk.Encoders.Binary
+{
+    public interface IBinaryDecoder
+    {
+        bool Decode(Stream inStream, Stream outStream, int version);
+        int MaxVersion { get; }
+    }
+}
---
+++ src/Net.Chdk.Encoders.Binary.Core/IBinaryEncoder.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace Net.Chdk.Encoders.Binary
+{
+    public interface IBinaryEncoder
+    {
+        void Encode(Stream inStream, Stream outStream, int version);
+        int MaxVersion { get; }
+    }
+}
