6caaf19298aa4a7904118b5e5e50223b5aa0df5a
Dmitry Shechtman <damnian@gmail.com>
2017-05-13 20:55:20 +0300

    Add buffer Encode() and Decode()

--- src/Net.Chdk.Encoders.Binary.Core/IBinaryEncoder.cs
+++ src/Net.Chdk.Encoders.Binary.Core/IBinaryEncoder.cs
@@ -5,6 +5,7 @@ namespace Net.Chdk.Encoders.Binary
     public interface IBinaryEncoder
     {
         void Encode(Stream inStream, Stream outStream, int version);
+        void Encode(byte[] decBuffer, byte[] encBuffer, int version);
         int MaxVersion { get; }
     }
 }
