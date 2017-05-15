5e554da14ffe2e29a5d3b3362740254d2f1cbe85
Dmitry Shechtman <damnian@gmail.com>
2017-05-15 13:33:29 +0300

    Hybrid approach

--- src/Net.Chdk.Encoders.Binary.Core/IBinaryDecoder.cs
+++ src/Net.Chdk.Encoders.Binary.Core/IBinaryDecoder.cs
@@ -4,7 +4,7 @@ namespace Net.Chdk.Encoders.Binary
 {
     public interface IBinaryDecoder
     {
-        bool Decode(Stream encStream, Stream decStream, int version);
+        bool Decode(Stream encStream, Stream decStream, ulong? offsets);
         bool Decode(byte[] encBuffer, byte[] decBuffer, ulong[] ulBuffer, ulong? offsets);
         int MaxVersion { get; }
     }
--- src/Net.Chdk.Encoders.Binary.Core/IBinaryEncoder.cs
+++ src/Net.Chdk.Encoders.Binary.Core/IBinaryEncoder.cs
@@ -4,7 +4,7 @@ namespace Net.Chdk.Encoders.Binary
 {
     public interface IBinaryEncoder
     {
-        void Encode(Stream decStream, Stream encStream, int version);
+        void Encode(Stream decStream, Stream encStream, ulong? offsets);
         void Encode(byte[] decBuffer, byte[] encBuffer, ulong[] ulBuffer, ulong? offsets);
         int MaxVersion { get; }
     }
